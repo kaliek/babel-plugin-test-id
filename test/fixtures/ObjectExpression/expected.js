9ea52b29dc796231b27473965bd138ac331eeb41
xinhuixinhuii <xinhuixinhuii@gmail.com>
2023-03-01 16:02:49 +0800

    [SF-3958] Added test cases for ObjectExpression

---
+++ test/fixtures/ObjectExpression/expected.js
@@ -0,0 +1,11 @@
+const Bar = () => (
+    <div 
+      header={{  
+        title: t("DASHBOARD_CURRENT_HVAC_EFFICIENCY"),  
+        link: { path: HvacPage.ChillerPlantMonitoring }
+      }} 
+      data-bd-fe-id="Hi-DASHBOARD_CURRENT_HVAC_EFFICIENCY"
+    >
+      Hi
+    </div>
+);
